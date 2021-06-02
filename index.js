f25fce43203d9784278f85106060d4c0fd48421a
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-01 19:55:26 -0500

    Send a json to the server

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const express = require("express");
+import express from "express";
+import Products from "./products.js";
 const app = express();
 const port = 3000;
 
+// GET
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  // res.send("Hello Node WORLD");
+  res.json(Products);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log("Listening in port " + port);
 });
---
+++ products.js
@@ -0,0 +1,16 @@
+const data = [
+  {
+    id: 1,
+    name: "Rice",
+  },
+  {
+    id: 2,
+    name: "Cookies",
+  },
+  {
+    id: 3,
+    name: "Pasta",
+  },
+];
+
+export default data;
