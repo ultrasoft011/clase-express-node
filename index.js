f9ff56343d0c6df56b0ffc5db02e5760693ed234
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-01 22:49:50 -0500

    Post request

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 import express from "express";
-import Products from "./products.js";
+// import Products from "./products.js";
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false
+}));
+
+
+const data = [
+  {
+    id: 1,
+    name: "Rice",
+  },
+  {
+    id: 2,
+    name: "Cookies",
+  },
+  {
+    id: 3,
+    name: "Pasta",
+  },
+];
+
 
 // GET
 
 app.get("/", (req, res) => {
   // res.send("Hello Node WORLD");
-  res.json(Products);
+  res.json(data);
 });
 
-app.listen(port, () => {
-  console.log("Listening in port " + port);
+// POST
+// Any action that requires creation of a resource. E.g: Creating a new user account, new order.
+// Can allow data to be sent along with in a form of a request body. Are a type of HTTP message that is sent by the client to the server
+
+ app.post("/", function (req, res) {
+   const newData = req.body;
+   data.push(newData);
+   res.send("POST request to the homepage");
+ });
+
+ app.listen(port, () => {
+   console.log("Listening in port " + port);
 });
