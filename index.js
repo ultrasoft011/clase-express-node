2a34b685af549089ca4083a50cd3956ef50b426a
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-11 19:52:44 -0500

    feature/put

--- index.js
+++ index.js
@@ -36,16 +36,8 @@ app.get("/data", (req, res) => {
   res.status(200).send(data);
 });
 
-// POST
-// Any action that requires creation of a resource. E.g: Creating a new user account, new order.
-// Can allow data to be sent along with in a form of a request body. Are a type of HTTP message that is sent by the client to the server
+// PUT
 
-app.post("/", (req, res) => {
-  const food = req.body;
-  data.push(food);
-  res.send("POST request to the homepage");
-});
-
-app.listen(port, () => {
-  console.log("Listening in port " + port);
-});
+app.put('/', function (req, res) {
+  res.send('Information to put')
+})
